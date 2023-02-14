bbbdb6ec57b89685fb1ebef0391679227393f577
melsavetisyan <109077883+melsavetisyan@users.noreply.github.com>
2023-02-14 05:19:45 +0400

    2.12.2023 5:19

--- src/components/main/paymant/Payment.jsx
+++ src/components/main/paymant/Payment.jsx
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+// import CreditCardInput from 'react-credit-card-input'
+
 import * as yup from 'yup';
 
 import styles from './payment.module.css'
@@ -19,6 +21,20 @@ export default function Payment(){
   //   CVC: string().url().nullable(),
   //   createdOn: date().default(() => new Date()),
   // });
+  const [changedCard, setChangedCard] = useState(formData.CARD_NUMBER)
+  useEffect(() => {
+    setChangedCard(
+      String(formData.CARD_NUMBER).split('').map((figure, index) =>{
+        if((index+1)%4===0 && index>0   ){
+              console.log(figure, index)
+            return `${figure} `
+        }
+        return figure
+      }
+    ).join(''))
+  }, [formData])
+  
+
   window.data = formData
 
   const handleSubmit = (event) => {
@@ -27,25 +43,24 @@ export default function Payment(){
   };
   const inputChangeFn = (e, name) =>{
     // setFormData({...formData, [name]:e.target.value })
-    
+
     switch(name){
       case "CARD_NUMBER":
-      if(!isNaN(+e.target.value.split(' ').join(""))){
-        
-        if(e.target.value.length<20){
-          setFormData({...formData, [name]:e.target.value})          
-          if(formData[name].length%4 === 0 && formData[name]>3){
-            setFormData({...formData, [name]:formData[name]+' '})
+        if(changedCard.split('').reverse[0] === " "){
+          
+        }
+        if(!isNaN(+e.target.value.split(' ').join(""))){
+          if(e.target.value.length<20){
+            setFormData({...formData, [name]:e.target.value.split(' ').join("")})          
           }
         }
-      }
     }
   }
 
   return (
     <form className={styles.form} onSubmit={handleSubmit} >
       <div className={styles.formDiv} >
-        <input className={styles.input} onInput={(e)=>inputChangeFn(e, "CARD_NUMBER")} type='tel'  value={formData.CARD_NUMBER} placeholder="Card Number"/>
+        <input className={styles.input} onInput={(e)=>inputChangeFn(e, "CARD_NUMBER")} type='tel'  value={changedCard} placeholder="Card Number"/>
         <input className={styles.input} onInput={(e)=>inputChangeFn(e, "MM_YY")} type="tel"   value={formData.MM_YY} placeholder="MM/YY" />
         <input className={styles.input} onInput={(e)=>inputChangeFn(e, "E_MAIL")} type='email' value={formData.E_MAIL}  placeholder="E-Mail" />
         <input className={styles.input} onInput={(e)=>inputChangeFn(e, "CVC")} type="password"  value={formData.CVC} placeholder="CVC" />
